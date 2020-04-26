044e7cc247b0569cae0d2d75d1aeac1a44afa439
Chas Berndt <yz80nomo@gmail.com>
2020-04-26 19:11:02 -0400

    Adds install-wordpress role. Numerous minor tweaks.

--- playbooks/configure-site.sh
+++ playbooks/configure-site.sh
@@ -3,4 +3,4 @@
 ansible-playbook create-infra.yml
 echo "Waiting for infrastructure to be accessible..."
 sleep 30
-ansible-playbook -i ../hosts configure-site.yml
+ansible-playbook -i ../hosts configure-infra.yml
